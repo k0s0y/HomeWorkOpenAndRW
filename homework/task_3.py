87be4cd34afff935b51e23d77ccce922d7fdb24f
Timofei <t.a.maksimenko@ya.ru>
2022-05-23 18:07:24 +0300

    Добавлен алгоритм внесения текста по количеству строк

--- homework/task_3.py
+++ homework/task_3.py
@@ -8,17 +8,32 @@ full_path = os.path.join(BASE_PATH, DIR_FILES)
 path_of_join = os.path.join(BASE_PATH, NAME_OF_NEW_FILE)
 
 
-def join_files(files_dir):
-    # создаем файл куда сохраняем остальные (в другой директории, что бы избежать рекурсии)
-    text_file = open(NAME_OF_NEW_FILE, "w")
-
+def make_list_of_file(files_dir):
+    file_list = {}
+    sorted_dict = {}
     list_of_file = os.listdir('other_files')
     for file in list_of_file:
         file_path = os.path.join(files_dir, file)
         with open(file_path, 'r') as doc:
             lines = doc.readlines()
+            file_list[file] = len(lines)
+    sorted_values = sorted(file_list.values())
+    for i in sorted_values:
+        for k in file_list.keys():
+            if file_list[k] == i:
+                sorted_dict[k] = file_list[k]
+                break
+    return sorted_dict
+
+
+def join_files(files_dir):
+    # создаем файл куда сохраняем остальные (в другой директории, что бы избежать рекурсии)
+    list_of_file = make_list_of_file(files_dir)
+    for file in list_of_file:
+        file_path = os.path.join(files_dir, file)
+        with open(file_path, 'r') as doc:
+            lines = doc.readlines()
             for line in lines:
-                print(line.strip())
                 with open(path_of_join, 'a') as new_file:
                     new_file.write(line)
 
